e49f4312d378cf67a13f3483ee4f2bc6b7cf0bbb
a <a@b.ch>
2020-07-19 00:05:18 +0200

    move styles to scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import Grid from "./components/usersGrid";
+import { Header } from "./components/PageHeader";
 
 function App() {
   return (
     <div className="App">
-      <Grid />
+      <div className="app-header">
+        <Header />
+      </div>
+      <div className="app-content">
+        <Grid />
+      </div>
     </div>
   );
 }
