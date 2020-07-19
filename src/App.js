71687b7c65968b2a66a5ecd55254a1b3aa2f0c70
a <a@b.ch>
2020-07-19 13:11:17 +0200

    Add Routing
    Add routing to the App and move the loading of the initial batch of users into the App component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/App.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import Grid from "./components/usersGrid";
-import { Header } from "./components/PageHeader";
-import { Provider } from "react-redux";
-import { store } from "./redux";
+import { Header, pages } from "./components/PageHeader";
+import { loadNextBatch } from "./services/userService";
+
+// Load the first batch of users as the app starts
+loadNextBatch();
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <div className="app-header">
-          <Header />
-        </div>
-        <div className="app-content">
-          <Grid />
-        </div>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
+              <div className="app-header">
+                <Header page={pages.usersGrid} />
+              </div>
+              <div className="app-content">
+                <Grid />
+              </div>
+            </div>
+          </Route>
+          <Route exact path="/settings">
+            <div className="app-header">
+              <Header page={pages.settings} />
+            </div>
+            <div className="app-content">Add settings here</div>
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { loadNextBatch } from "./services/userService";
-
-// Load the first batch of users as the app starts
-loadNextBatch();
 
 ReactDOM.render(<App />, document.getElementById("root"));
